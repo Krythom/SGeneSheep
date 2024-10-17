8b398fe55a1910d020132d1f80ce48e6108aeada
Krythom <krythomia@gmail.com>
2024-10-17 16:18:23 -0300

    cleanup and new very simple but nice colour mutation rule

--- SGeneSheep/GeneSheep.cs
+++ SGeneSheep/GeneSheep.cs
@@ -5,7 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using SColor = System.Drawing.Color;
+using Bitmap = System.Drawing.Bitmap;
 
 namespace SGeneSheep
 {
@@ -17,11 +22,23 @@ namespace SGeneSheep
         Texture2D square;
         Sheep[,] world;
         List<Sheep> species = new();
-        int worldSize;
-        int cellSize;
-        int numSpecies;
+
+        bool completed = false;
+        bool saved = false;
+        int iterations = 0;
         HashSet<Point> checkSet = new();
-        const int colorVariation = 1;
+        Mutator mutator;
+
+        int worldX = 300;
+        int worldY = 300;
+        int numSpecies = 10;
+        const double Tolerance = 10;
+        const int CellSize = 3;
+        const int ColorVariation = 1;
+        const int MaxIterations = -1;
+        const bool BatchMode = false;
+        const bool ShowBorders = false;
+        const bool FromImage = false;
 
         public GeneSheep()
         {
@@ -45,14 +62,9 @@ namespace SGeneSheep
 
         protected override void Initialize()
         {
-            worldSize = 600;
-            cellSize = 1;
-            numSpecies = 10;
-            world = new Sheep[worldSize, worldSize];
             InitWorld();
-
-            _graphics.PreferredBackBufferHeight = worldSize * cellSize;
-            _graphics.PreferredBackBufferWidth = worldSize * cellSize;
+            _graphics.PreferredBackBufferHeight = worldY * CellSize;
+            _graphics.PreferredBackBufferWidth = worldX * CellSize;
             _graphics.SynchronizeWithVerticalRetrace = false;
             _graphics.ApplyChanges();
             base.Initialize();
@@ -66,23 +78,51 @@ namespace SGeneSheep
 
         protected override void Update(GameTime gameTime)
         {
-            Iterate();
+            if (completed)
+            {
+                if (!saved)
+                {
+                    SaveImage();
+                    saved = true;
+                }
+                if (BatchMode)
+                {
+                    completed = false;
+                    saved = false;
+                    Initialize();
+                    iterations = 0;
+                }
+            }
+            else
+            {
+                Iterate();
+                iterations++;
+            }
+
             double ms = gameTime.ElapsedGameTime.TotalMilliseconds;
-            Debug.WriteLine("fps: " + (1000/ms) + "    (" + ms + "ms)");
+            Debug.WriteLine("fps: " + (1000/ms) + " (" + ms + "ms)" + " iterations: " + iterations + "/" + MaxIterations + " active " + checkSet.Count);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            
             _spriteBatch.Begin();
 
-            for (int x = 0; x < worldSize; x++)
+            for (int x = 0; x < worldX; x++)
             {
-                for (int y = 0; y < worldSize; y++)
+                for (int y = 0; y < worldY; y++)
                 {
-                    Color squareColor = world[x,y].color;
-                    Rectangle squarePos = new(new Point((x * cellSize), (y * cellSize)), new Point(cellSize, cellSize));
-                    _spriteBatch.Draw(square, squarePos, squareColor);
+                    if (checkSet.Contains(new Point(x, y)) && ShowBorders)
+                    {
+                        Rectangle squarePos = new(new Point((x * CellSize), (y * CellSize)), new Point(CellSize, CellSize));
+                        _spriteBatch.Draw(square, squarePos, new Color(255,255,255));
+                    }
+                    else
+                    {
+                        Rectangle squarePos = new(new Point((x * CellSize), (y * CellSize)), new Point(CellSize, CellSize));
+                        _spriteBatch.Draw(square, squarePos, world[x, y].color);
+                    }
                 }
             }
 
@@ -92,27 +132,68 @@ namespace SGeneSheep
 
         private void InitWorld()
         {
-            for (int i = 0; i < numSpecies; i++)
+            if (FromImage)
             {
-                species.Add(new Sheep(new Color(rand.Next(256), rand.Next(256), rand.Next(256)), i));
-            }
-            for (int x = 0; x < worldSize; x++)
-            {
-                for (int y = 0; y < worldSize; y++)
+                if (!Directory.Exists(@"Images"))
                 {
-                    world[x, y] = species[rand.Next(numSpecies)].DeepCopy();
-                    world[x, y].x = x;
-                    world[x, y].y = y;
-                    checkSet.Add(new Point(x, y));
+                    Directory.CreateDirectory(@"Images");
+                }
+                try
+                {
+                    Bitmap img = new(@"Images\input.png");
+                    Color sample = new(img.GetPixel(0, 0).R, img.GetPixel(0, 0).G, img.GetPixel(0, 0).B);
+                    worldX = img.Width;
+                    worldY = img.Height;
+                    world = new Sheep[worldX, worldY];
+
+                    for (int x = 0; x < worldX; x++)
+                    {
+                        for (int y = 0; y < worldY; y++)
+                        {
+                            Color col = new(img.GetPixel(x, y).R, img.GetPixel(x, y).G, img.GetPixel(x, y).B);
+                            int steps = (int)(GetDistance(sample, col) / Tolerance);
+                            if (steps >= numSpecies)
+                            {
+                                numSpecies = steps + 1;
+                            }
+                            world[x, y] = new Sheep(col, steps);
+                            world[x, y].x = x;
+                            world[x, y].y = y;
+                            checkSet.Add(new Point(x, y));
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.WriteLine(e.Message);
+                }
+            }
+            else
+            {
+                world = new Sheep[worldX, worldY];
+                species.Clear();
+                for (int i = 0; i < numSpecies; i++)
+                {
+                    species.Add(new Sheep(Mutator.uniformCol, i));
+                }
+                for (int x = 0; x < worldX; x++)
+                {
+                    for (int y = 0; y < worldY; y++)
+                    {
+                        world[x, y] = species[rand.Next(numSpecies)].DeepCopy();
+                        world[x, y].x = x;
+                        world[x, y].y = y;
+                        checkSet.Add(new Point(x, y));
+                    }
                 }
             }
         }
 
         private void Iterate()
         {
-            Sheep[,] newWorld = new Sheep[worldSize, worldSize];
+            Sheep[,] newWorld = new Sheep[worldX, worldY];
 
-            List<SheepChange> changed = new() { Capacity = (int)Math.Pow(worldSize, 2) };
+            List<SheepChange> changed = new() { Capacity = worldX * worldY};
             HashSet<Point> toSleep = new();
             HashSet<Point> toWake = new();
 
@@ -131,7 +212,7 @@ namespace SGeneSheep
                 }
                 else if (winningSpecies != s.species)
                 {
-                    changed.Add(new() { color = GetNewColor(s, winningSpecies), id = winningSpecies, loc = loc });
+                    changed.Add(new() { color = Mutator.Uniform(), id = winningSpecies, loc = loc });
                     lock (toWake)
                     {
                         toWake.UnionWith(GetNeighbours(s).Select(x => { return x.GetPoint(); }));
@@ -145,22 +226,31 @@ namespace SGeneSheep
                 s.species = c.id;
                 s.color = c.color;
             }
+            if (changed.Count == 0 || iterations == MaxIterations)
+            {
+                completed = true;
+            }
+
             checkSet.UnionWith(toWake);
             checkSet.ExceptWith(toSleep);
+
+            Mutator.uniformCol = new Color(Mutator.uniformCol.R + rand.Next(-ColorVariation, ColorVariation + 1),
+                                           Mutator.uniformCol.G + rand.Next(-ColorVariation, ColorVariation + 1),
+                                           Mutator.uniformCol.B + rand.Next(-ColorVariation, ColorVariation + 1));
         }
 
         private List<Sheep> GetNeighbours(Sheep s)
         {
             List<Sheep> sheep = new()
             {
-                world[Mod(s.x + -1, worldSize), Mod(s.y + -1, worldSize)],
-                world[Mod(s.x + 0, worldSize), Mod(s.y + -1, worldSize)],
-                world[Mod(s.x + 1, worldSize), Mod(s.y + -1, worldSize)],
-                world[Mod(s.x + -1, worldSize), Mod(s.y , worldSize)],
-                world[Mod(s.x + 1, worldSize), Mod(s.y, worldSize)],
-                world[Mod(s.x + -1, worldSize), Mod(s.y + 1, worldSize)],
-                world[Mod(s.x + 0, worldSize), Mod(s.y + 1, worldSize)],
-                world[Mod(s.x + 1, worldSize), Mod(s.y + 1, worldSize)],
+                world[Mod(s.x + -1, worldX), Mod(s.y + -1, worldY)],
+                world[Mod(s.x + 0, worldX), Mod(s.y + -1, worldY)],
+                world[Mod(s.x + 1, worldX), Mod(s.y + -1, worldY)],
+                world[Mod(s.x + -1, worldX), Mod(s.y, worldY)],
+                world[Mod(s.x + 1, worldX), Mod(s.y, worldY)],
+                world[Mod(s.x + -1, worldX), Mod(s.y + 1, worldY)],
+                world[Mod(s.x + 0, worldX), Mod(s.y + 1, worldY)],
+                world[Mod(s.x + 1, worldX), Mod(s.y + 1, worldY)],
 
             };
             return sheep;
@@ -175,10 +265,10 @@ namespace SGeneSheep
             {
                 for (int y = -1; y <= 1; y++)
                 {
-                    int getX = Mod(x + sheep.x, worldSize);
-                    int getY = Mod(y + sheep.y, worldSize);
+                    int getX = Mod(x + sheep.x, worldX);
+                    int getY = Mod(y + sheep.y, worldY);
                     if (!(getX == sheep.x && getY == sheep.y))
-            {
+                    {
                         neighborSpecies[world[getX, getY].species]++;
                     }
                 }
@@ -209,26 +299,33 @@ namespace SGeneSheep
             return winningSpecies;
         }
 
-        private Color GetNewColor(Sheep sheep, int winner)
+        private void SaveImage()
         {
-            List<Sheep> winners = new();
-            for (int x = -1; x <= 1; x++)
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                for (int y = -1; y <= 1; y++)
+                Bitmap img = new Bitmap(worldX, worldY);
+
+                for (int x = 0; x < worldX; x++)
                 {
-                    int getX = Mod(x + sheep.x, worldSize);
-                    int getY = Mod(y + sheep.y, worldSize);
-                    if (world[getX, getY].species == winner)
+                    for (int y = 0; y < worldY; y++)
                     {
-                        winners.Add(world[getX, getY]);
+                        img.SetPixel(x, y, ConvertColor(world[x, y].color));
                     }
                 }
+
+                img.Save(@"ScreenCap_n" + numSpecies + "_v" + ColorVariation + "_i" + iterations + ".png", System.Drawing.Imaging.ImageFormat.Png);
             }
-            Color newCol = winners[0].color;
-            newCol.R = (byte)(Math.Clamp(newCol.R + rand.Next(-colorVariation, colorVariation + 1), 0, 255));
-            newCol.G = (byte)(Math.Clamp(newCol.G + rand.Next(-colorVariation, colorVariation + 1), 0, 255));
-            newCol.B = (byte)(Math.Clamp(newCol.B + rand.Next(-colorVariation, colorVariation + 1), 0, 255));
-            return newCol;
+        }
+
+        private static SColor ConvertColor(Color col)
+        {
+            SColor sCol = SColor.FromArgb(255, col.R, col.G, col.B);
+            return sCol;
+        }
+
+        private static double GetDistance(Color sample, Color col)
+        {
+            return Math.Pow(Math.Pow(sample.R - col.R, 2) + Math.Pow(sample.G - col.G, 2) + Math.Pow(sample.B - col.B, 2), 0.5);
         }
 
         private static int Mod(int x, int m)
---
+++ SGeneSheep/Mutator.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace SGeneSheep
+{
+    internal class Mutator
+    {
+        static int[] dis = new[] { -1, -1, -1, 0, 1, 1, 1, 0 };
+        static int[] djs = new[] { -1, 0, 1, 1, 1, 0, -1, -1 };
+        public static int mutationStrength;
+        static Random rand = new();
+        public static Color uniformCol = new Color(rand.Next(256), rand.Next(256), rand.Next(256));
+
+        //At an offset of 4 makes spiral structures, as offset gets closer to 0 spirals become less distinct 
+        public static Color Spiral(Sheep[,] world, Sheep sheep, int winner, int offset)
+        {
+            Color newCol;
+            int worldX = world.GetLength(0);
+            int worldY = world.GetLength(1);
+            bool prev = world[Mod(sheep.x + dis[^1], worldX), Mod(sheep.y + djs[^1], worldY)].species == winner;
+            int start = -1;
+            int end = -1;
+
+            for (int index = 0; index < 8; ++index)
+            {
+                bool curr = world[Mod(sheep.x + dis[index], worldX), Mod(sheep.y + djs[index], worldY)].species == winner;
+                if (curr)
+                {
+                    if (!prev)
+                    {
+                        //found first winner
+                        start = index;
+                        if (end >= 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (prev)
+                {
+                    //past last winner
+                    end = index;
+                    if (start >= 0)
+                    {
+                        break;
+                    }
+                }
+                prev = curr;
+            }
+
+            int length = Mod(end - start, 8);
+            int midpointIndex = start + length / 2;
+            if (Mod(length, 2) == 0)//even # of winners
+            {
+                midpointIndex -= rand.Next(0, 2);
+            }
+            if (length >= 3)
+            {
+                midpointIndex += rand.Next(-1, 2);
+            }
+            midpointIndex += offset;
+            midpointIndex = Mod(midpointIndex, 8);
+
+            if (world[Mod(sheep.x + dis[midpointIndex], worldX), Mod(sheep.y + djs[midpointIndex], worldY)].species == winner)
+            {
+                newCol = world[Mod(sheep.x + dis[midpointIndex], worldX), Mod(sheep.y + djs[midpointIndex], worldY)].color;
+            }
+            else
+            {
+                newCol = world[Mod(sheep.x + dis[start], worldX), Mod(sheep.y + djs[start], worldY)].color;
+            }
+
+            newCol.R = (byte)(Math.Clamp(newCol.R + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.G = (byte)(Math.Clamp(newCol.G + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.B = (byte)(Math.Clamp(newCol.B + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            return newCol;
+        }
+
+        public static Color Uniform()
+        {
+            return uniformCol;
+        }
+
+        public static Color Preservation(Sheep[,] world, Sheep sheep, int winner)
+        {
+            Color newCol;
+            List<Sheep> neighbors = GetNeighbours(world, sheep, true);
+            int lowest = 900;
+            int lowestIndex = 0;
+            int current = 0;
+
+            foreach (Sheep s in neighbors)
+            {
+                int sum = Math.Abs(s.color.R - sheep.color.R) + Math.Abs(s.color.G - sheep.color.G) + Math.Abs(s.color.B - sheep.color.B);
+                if (sum < lowest && s.species == winner)
+                {
+                    lowest = sum;
+                    lowestIndex = current;
+                }
+                current++;
+            }
+
+            newCol = neighbors[lowestIndex].color;
+            newCol.R = (byte)(Math.Clamp(newCol.R + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.G = (byte)(Math.Clamp(newCol.G + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.B = (byte)(Math.Clamp(newCol.B + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            return newCol;
+        }
+
+        public static Color Mirror(Sheep[,] world, Sheep sheep, int winner)
+        {
+            Color newCol;
+            int mirrorX;
+            int mirrorY;
+            mirrorX = world.GetLength(0) - sheep.x - 1;
+            mirrorY = world.GetLength(1) - sheep.y - 1;
+
+            if (world[mirrorX, mirrorY].species == winner)
+            {
+                newCol = world[mirrorX, mirrorY].color;
+            }
+            else
+            {
+                newCol = sheep.color;
+                List<Sheep> neighbors = GetNeighbours(world, world[mirrorX, mirrorY], true);
+                foreach (Sheep neighbor in neighbors)
+                {
+                    if (neighbor.species == winner)
+                    {
+                        newCol = neighbor.color;
+                        break;
+                    }
+                }
+            }
+            newCol.R = (byte)(Math.Clamp(newCol.R + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.G = (byte)(Math.Clamp(newCol.G + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            newCol.B = (byte)(Math.Clamp(newCol.B + rand.Next(-mutationStrength, mutationStrength + 1), 0, 255));
+            return newCol;
+        }
+
+        private static List<Sheep> GetNeighbours(Sheep[,] world, Sheep s, bool shuffled)
+        {
+            int worldX = world.GetLength(0);
+            int worldY = world.GetLength(1);
+            List<Sheep> sheep = new()
+            {
+                world[Mod(s.x + -1, worldX), Mod(s.y + -1, worldY)],
+                world[Mod(s.x + 0, worldX) , Mod(s.y + -1, worldY)],
+                world[Mod(s.x + 1, worldX) , Mod(s.y + -1, worldY)],
+                world[Mod(s.x + -1, worldX), Mod(s.y , worldY)],
+                world[Mod(s.x + 1, worldX) , Mod(s.y, worldY)],
+                world[Mod(s.x + -1, worldX) , Mod(s.y + 1, worldY)],
+                world[Mod(s.x + 0, worldX) , Mod(s.y + 1, worldY)],
+                world[Mod(s.x + 1, worldX) , Mod(s.y + 1, worldY)],
+
+            };
+
+            if (shuffled)
+            {
+                int n = sheep.Count;
+                while (n > 1)
+                {
+                    n--;
+                    int k = rand.Next(n + 1);
+                    Sheep temp = sheep[k];
+                    sheep[k] = sheep[n];
+                    sheep[n] = temp;
+                }
+            }
+            return sheep;
+        }
+
+        private static int Mod(int x, int m)
+        {
+            return (Math.Abs(x * m) + x) % m;
+        }
+    }
+}
